4f6c912dbcb531a36f107d8149efcd55a11693b4
cscully-allison <cscully-allison@nevada.unr.edu>
2018-10-18 23:25:55 -0700

    for merge

--- Parser/Source.c
+++ Parser/Source.c
@@ -7,31 +7,31 @@ int main(int a, char b){
   int a, c, d;
 
 
-if (2>5)
-{
-	int a;
-	int q;
-	char c;
-	q = c+a;
-}
-
-if (2>5)
-{
-	int a;
-	int q;
-	char c;
-
-	q = c+a;
-
-  for(i = 0; i < 3; i++){
-
-    float a;
-
-    a = b + c;
-    b += 9;
-
-
+  if (2>5)
+  {
+  	int a;
+  	int q;
+  	char c;
+  	q = c+a;
   }
-}
+
+  if (2>5)
+  {
+  	int a;
+  	int q;
+  	char c;
+
+  	q = c+a;
+
+      for(i = 0; i < 3; i++){
+
+        float a;
+
+        a = b + c;
+        b += 9;
+
+
+      }
+  }
 
 }
