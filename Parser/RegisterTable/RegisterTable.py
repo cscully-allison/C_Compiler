30820991b9ada6114687cea7da65394417bfcb2a
kandersen753 <kurt.t.andersen@gmail.com>
2018-11-18 20:59:12 -0800

    add into register table and overflow call working

--- Parser/RegisterTable/RegisterTable.py
+++ Parser/RegisterTable/RegisterTable.py
@@ -1,38 +1,38 @@
 class RegisterTable():
 	def __init__ (self):
 		self.Registers = []
-		self.Registers.append({'register': 'r0', 'assembly name': '$zero', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r1', 'assembly name': '$at', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r2', 'assembly name': '$v0', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r3', 'assembly name': '$v1', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r4', 'assembly name': '$a0', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r5', 'assembly name': '$a1', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r6', 'assembly name': '$a2', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r7', 'assembly name': '$a3', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r8', 'assembly name': '$t0', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r9', 'assembly name': '$t1', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r10', 'assembly name': '$t2', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r11', 'assembly name': '$t3', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r12', 'assembly name': '$t4', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r13', 'assembly name': '$t5', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r14', 'assembly name': '$t6', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r15', 'assembly name': '$t7', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r16', 'assembly name': '$s0', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r17', 'assembly name': '$s1', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r18', 'assembly name': '$s2', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r19', 'assembly name': '$s3', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r20', 'assembly name': '$s4', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r21', 'assembly name': '$s5', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r22', 'assembly name': '$s6', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r23', 'assembly name': '$s7', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r24', 'assembly name': '$t8', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r25', 'assembly name': '$t9', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r26', 'assembly name': '$k0', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r27', 'assembly name': '$k1', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r28', 'assembly name': '$gp', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r29', 'assembly name': '$sp', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r30', 'assembly name': '$fp', 'value': None, 'data type' : None})
-		self.Registers.append({'register': 'r31', 'assembly name': '$ra', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$zero', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$at', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$v0', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$v1', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$a0', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$a1', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$a2', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$a3', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$t0', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$t1', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$t2', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$t3', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$t4', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$t5', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$t6', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$t7', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$s0', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$s1', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$s2', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$s3', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$s4', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$s5', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$s6', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$s7', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$t8', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$t9', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$k0', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$k1', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$gp', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$sp', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$fp', 'value': None, 'data type' : None})
+		self.Registers.append({'assembly name': '$ra', 'value': None, 'data type' : None})
 
 	def GetRegisterData(self, AssemblyName = None):
 		if AssemblyName is not None:
@@ -43,8 +43,47 @@ class RegisterTable():
 			return False
 
 	def SetRegisterData(self, AssemblyName = None, NewValue = None, NewDataType = None):
-		if AssemblyName is not None:
-			for elem in self.Registers:
-				if elem['assembly name'] == AssemblyName:
-					elem['value'] = NewValue
-					elem['data type'] = NewDataType
+		for elem in self.Registers:
+			if elem['assembly name'] == AssemblyName:
+				elem['value'] = NewValue
+				elem['data type'] = NewDataType
+				return True
+
+		#no register found
+		return False
+
+
+
+
+	def SetSRegister(self, NewValue = None, NewDataType = None):
+		RegisterName = self.GetFirstOpenRegister('$s')
+		if RegisterName is not False:
+			self.SetRegisterData(RegisterName, NewValue, NewDataType)
+		else:
+			self.RegisterOverflow()
+	
+	def SetTRegister(self, NewValue = None, NewDataType = None):
+		RegisterName = self.GetFirstOpenRegister('$t')
+		if RegisterName is not False:
+			self.SetRegisterData(RegisterName, NewValue, NewDataType)
+		else:
+			self.RegisterOverflow()
+
+
+	def SetARegister(self, NewValue = None, NewDataType = None):
+		RegisterName = self.GetFirstOpenRegister('$a')
+		if RegisterName is not False:
+			self.SetRegisterData(RegisterName, NewValue, NewDataType)
+		else:
+			self.RegisterOverflow()
+
+	def RegisterOverflow(self):
+		print('Register Overflow Called')
+		pass
+	
+	def GetFirstOpenRegister(self, RegisterType = None):
+		for elem in self.Registers:
+			if RegisterType in elem['assembly name'] and elem['assembly name'] != '$at' and elem['assembly name'] != '$sp':
+				if elem['value'] is None and elem['data type'] is None:
+					return elem['assembly name']
+		return False
