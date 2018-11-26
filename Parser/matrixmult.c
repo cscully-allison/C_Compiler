7a9954db5a1ba5f1253070829637ae5d4409ac2b
Connor Scully-allison <cscully-allison@unr.edu>
2018-11-25 19:27:32 -0800

    Matrix mult works

---
+++ Parser/matrixmult.c
@@ -0,0 +1,24 @@
+
+int main(){
+  int i, j, k;
+  float a[10][10];
+  float b[20][5];
+  float c[10][9][8];
+
+  if( i < j){
+    c[j][i][k];
+  }
+  else {
+    i = superlongvariablename + 4 ;
+  }
+
+  for(i=0; i < 10; i++){
+    for(j=0; j < 10; j++){
+      for(k=0; k < 10; k++){
+          c[j][i][k];
+      }
+    }
+  }
+
+  return 0;
+}
