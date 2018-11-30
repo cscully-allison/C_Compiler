c17c73bfca8c3469cce1c1b2282a50273c8927fe
kandersen753 <kurt.t.andersen@gmail.com>
2018-11-29 23:24:08 -0800

    offsets fixed in bubbleSort.c

--- Parser/bubbleSort.c
+++ Parser/bubbleSort.c
@@ -4,20 +4,18 @@ int main()
 	int b;
 	int vals[10];
 	int c;
-	int d;
 
 	for (a=0; a<10; a++)
 	{
 
 		for (b=0; b<9; b++)
 		{
-			d = b + 1;
-			if (vals[b] > vals[d])
+			if (vals[b] > vals[b + 1])
 			{
 
 				c = vals[b];
-				vals[b] = vals[d];
-				vals[d]= c;
+				vals[b] = vals[b + 1];
+				vals[b + 1]= c;
 			}
 		}
 	}
