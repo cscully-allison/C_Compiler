d6688658a64c19f054146460f5e6795624e634aa
cscully-allison <cscully-allison@nevada.unr.edu>
2018-12-18 22:32:11 -0800

    fib

---
+++ Parser/.BasicSource.c
@@ -0,0 +1,34 @@
+int funct(){
+
+}
+
+int main(int g){
+    // $!PDO
+    unsigned char a, d;
+    // $!PDF
+    // $!PDO
+    int arr[8][10];
+    // $!PDF
+    int ndx;
+
+    if(a < d){
+      int a4;
+
+
+    } else {
+      long int b6;
+      b6 = 4 + (2 * ( 4/ 10) );
+      b6 = b6 + 2;
+
+          $!ST1
+    }
+
+    for(ndx=0; ndx < 10; ndx++){
+      int b4;
+    }
+
+    g = 2 + 3.4;
+    d = 5 * 5;
+    d = d >> 5;
+
+}
---
+++ Parser/connortest.c
@@ -0,0 +1,9 @@
+char globalvar;
+
+int main(){
+    int b, c, d;
+    b = 4;
+    c = 2;
+    d = 1;
+    b = (1 + d) < (2.2 - (globalvar * ( b / 4 ) ) );
+}
