287f97156fa58e62edac4268b53b65a5b60c88cb
cscully-allison <cscully-allison@nevada.unr.edu>
2018-12-18 23:29:23 -0800

    new mm

---
+++ Parser/mm.c
@@ -0,0 +1,41 @@
+int PrintInt(int);
+
+int main(){
+  int i, j, k;
+  int a[2][2];
+  int b[2][2];
+  int c[2][2];
+  int aval, bval;
+  int out;
+
+  aval = 3;
+  bval = 2;
+
+  for(i=0; i < 2; i++){
+    for(j=0; j < 2; j++){
+      a[i][j] = 3;
+
+      b[i][j] = 2;
+
+      c[i][j] = 0;
+    }
+  }
+
+  for(i=0; i < 2; i++){
+    for(j=0; j < 2; j++){
+      for(k=0; k < 2; k++){
+          c[i][k] += a[i][j] * b[j][k];
+      }
+    }
+  }
+
+  for(i=0; i < 2; i++){
+    for(j=0; j < 2; j++){
+      out = c[i][j];
+      PrintInt(out);
+    }
+  }
+
+
+  return 0;
+}
